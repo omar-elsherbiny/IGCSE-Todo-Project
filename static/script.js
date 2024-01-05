325e1916cd3f865913eb6769c70823cadd199136
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-01-05 20:03:01 +0200

    added bracket

--- static/script.js
+++ static/script.js
@@ -252,10 +252,10 @@ function boardsViewDrop(event) {
                         </svg>
                         <h5>${task.task}</h5>
                         <h4 class="prevent-select" style="color: rgba(0, 0, 0, 0.2); margin-left: 0.5rem;">|</h4>
-                        <svg id="task_check" onclick="doneTask(${board.board_id},${task.task_id}" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-                            <path fill="currentColor"
-                                d="m10 13.6l5.9-5.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7l-6.6 6.6q-.3.3-.7.3t-.7-.3l-2.6-2.6q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275z" />
-                        </svg>
+                        <svg id="task_check" onclick="doneTask(${board.board_id},${task.task_id})" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="m10 13.6l5.9-5.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7l-6.6 6.6q-.3.3-.7.3t-.7-.3l-2.6-2.6q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275z" />
+                    </svg>
                     </div>
                     <div class="dropzone dlist_item" style="display: none;">`
                     for (const li of task.list) {
@@ -369,4 +369,6 @@ function doneTask(board_id, task_id) {
     updateData({ 'rem_task': task_id, 'board_id': board_id });
     let t = document.querySelector('#board' + board_id + '.board_open #task' + task_id);
     if (t != null) t.style.animation = "nope_out 0.5s ease forwards";
+    let n = document.querySelector('#board' + board_id + '.board_closed h5');
+    n.textContent = Number(n.textContent) - 1;
 }
