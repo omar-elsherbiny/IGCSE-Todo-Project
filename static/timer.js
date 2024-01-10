5cdc5b878470925fe5f6abe722ddb08712fd2cf0
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-01-10 12:10:09 +0200

    timer +++++

--- static/timer.js
+++ static/timer.js
@@ -10,8 +10,8 @@ function startTimer() {
 
     elapsedTime = 0;
     paused = false;
-    document.querySelector('#timer h4').classList.toggle('hide');
-    document.querySelector('.timer_clock').classList.toggle('hide');
+    //document.querySelector('#timer h4').classList.toggle('hide');
+    //document.querySelector('.timer_clock').classList.toggle('hide');
     document.querySelector('#start_controls').classList.toggle('hide');
     document.querySelector('#playing_controls').classList.toggle('hide');
     document.querySelector('.timer_clock h5').style.animation = 'scale_bounce2 ease-in-out 1s infinite';
@@ -25,8 +25,8 @@ function startTimer() {
             updateTimerDisplay();
 
             if (elapsedTime >= timerDuration) {
-                document.querySelector('#timer h4').classList.toggle('hide');
-                document.querySelector('.timer_clock').classList.toggle('hide');
+                //document.querySelector('#timer h4').classList.toggle('hide');
+                //document.querySelector('.timer_clock').classList.toggle('hide');
                 document.querySelector('#start_controls').classList.toggle('hide');
                 document.querySelector('#playing_controls').classList.toggle('hide');
                 clearInterval(timer);
@@ -58,8 +58,8 @@ function resetTimer() {
     document.querySelector('.timer_clock h5').style.animation = '';
     document.querySelector('.timer_clock div').style.animation = '';
     updateTimerDisplay();
-    document.querySelector('#timer h4').classList.toggle('hide');
-    document.querySelector('.timer_clock').classList.toggle('hide');
+    //document.querySelector('#timer h4').classList.toggle('hide');
+    //document.querySelector('.timer_clock').classList.toggle('hide');
     document.querySelector('#start_controls').classList.toggle('hide');
     document.querySelector('#playing_controls').classList.toggle('hide');
 }
@@ -73,8 +73,8 @@ function updateTimerDisplay() {
 }
 
 function addTimer(time) {
-    timerDuration = timerDuration + time;
+    timerDuration = (timerDuration + time) > 0 ? (timerDuration + time) : timerDuration;
     const minutes = Math.floor(timerDuration / 60);
     const seconds = timerDuration % 60;
-    document.querySelector('#timer h4').textContent = minutes + ':' + (seconds < 10 ? seconds + '0' : seconds);
+    document.querySelector('#timer h5').textContent = minutes + ':' + (seconds < 10 ? seconds + '0' : seconds);
 }
