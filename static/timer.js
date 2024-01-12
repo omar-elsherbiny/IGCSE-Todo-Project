ed99a2c6f38f7e2ab751b8b62af95376988d7809
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-01-13 00:06:33 +0200

    timerfixes percentage refersh drop +9 cancel bug

--- static/timer.js
+++ static/timer.js
@@ -37,10 +37,11 @@ function startTimer() {
 
             if (elapsedTime >= timerDuration) {
                 audio.play();
+                let id=document.querySelector('header').childElementCount+1;
                 document.querySelector('header').innerHTML += `
-                <div class="alert" id="beep99">
+                <div class="alert" id="beep${id}">
                     <p>Timer Finished</p>
-                    <svg onclick="hide_alert(99);audio.pause();audio.currentTime=0;" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    <svg onclick="hide_alert(${id});audio.pause();audio.currentTime=0;" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         viewBox="0 0 512 512">
                         <path
                             d="M437.5 386.6L306.9 256l130.6-130.6c14.1-14.1 14.1-36.8 0-50.9-14.1-14.1-36.8-14.1-50.9 0L256 205.1 125.4 74.5c-14.1-14.1-36.8-14.1-50.9 0-14.1 14.1-14.1 36.8 0 50.9L205.1 256 74.5 386.6c-14.1 14.1-14.1 36.8 0 50.9 14.1 14.1 36.8 14.1 50.9 0L256 306.9l130.6 130.6c14.1 14.1 36.8 14.1 50.9 0 14-14.1 14-36.9 0-50.9z"
@@ -83,6 +84,8 @@ function resetTimer() {
     document.querySelector('#start_controls').classList.toggle('hide');
     document.querySelector('#playing_controls').classList.toggle('hide');
     document.querySelector('.timer_clock').style.borderColor = 'var(--accent)';
+    localStorage.setItem('timer_duration', 0);
+    document.querySelector('#timer').style = `--timer-start: 0`;
 }
 
 function updateTimerDisplay() {
@@ -116,8 +119,11 @@ if (localStorage.getItem('timer_duration')) {
     const first_epoch = Number(localStorage.getItem('timer_first_epoch'));
     const duration = Number(localStorage.getItem('timer_duration'));
     let timeLeft = duration - (currentTime() - first_epoch);
-    if (timeLeft>0) {
+    if (timeLeft > 0) {
+        document.querySelector('#timer').style = `--timer-start: ${100 * ((currentTime() - first_epoch) / duration)}%`;
         timerDuration = timeLeft;
         startTimer();
+    } else {
+        document.querySelector('#timer').style = `--timer-start: 0`;
     }
 }
